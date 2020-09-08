d6236ed85306b676728a1d6b8a2f10516a3a8b05
J0s <joshi.karthik@yahoo.com>
2020-09-09 00:55:28 +0530

    ice cream parlor problem

---
+++ src/IceCreamParlor.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class IceCreamParlor {
+
+    // Complete the whatFlavors function below.
+    static void whatFlavors(int[] cost, int money) {
+
+        Map<Integer, Integer> costmap = new HashMap<Integer, Integer>();
+        for(int i = 0 ; i < cost.length; i++){
+        	
+        	int value = money - cost[i];
+        	
+        	if(costmap.get(value) != null) {
+        		System.out.println(costmap.get(value)+" "+(i+1));
+        		break;
+        	}
+        	costmap.put(cost[i], i+1);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
